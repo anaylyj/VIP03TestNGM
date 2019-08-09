e58be8f9efcc3d735a089cd9e774e3d8752d08a9
anaylyj <1355214222@qq.com>
2019-08-09 20:08:46 +0800

    上传testng_allure项目 Signed-off-by: anaylyj <1355214222@qq.com>

---
+++ src/main/java/com/testing/UI/AutoLogger.java
@@ -0,0 +1,9 @@
+package com.testing.UI;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class AutoLogger {
+	public static final Logger log=LogManager.getLogger(AutoLogger.class);
+}
